800427537e4b1f6acced1d6a9abd80e6350b7d66
Nicholas Giambalvo <giambalvo03@gmail.com>
2025-04-16 11:36:29 -0500

    Added test class

---
+++ src/main/java/main/LandingPageTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LandingPageTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module main {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+	requires org.junit.jupiter.api;
+	requires junit;
+	requires jdk.incubator.vector;
     
     opens main to javafx.fxml;
     
